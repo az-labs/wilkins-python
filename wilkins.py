e1e66e546bb7312daf1f25c79a7d8febc1a70e51
Omar Elazhary <omazhary@gmail.com>
2016-08-28 22:14:03 +0200

    Differentiating between import and binary execution

---
+++ wilkins.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+class Wilkins:
+    """This class allows you to query words on WordNet's internet UI."""
+    i = 12345
+
+    def __init__(self):
+        print "This is the constructor!!"
+
+    def f(self):
+        return 'hello world'
+
+
+if __name__ == "__main__":
+    x = Wilkins()
+    print x.i
+    print x.f()
