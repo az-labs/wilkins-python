a8963225fa0478bbfac494f18db43a5d58fa73e8
Omar Elazhary <omazhary@gmail.com>
2016-09-04 20:16:14 -0700

    Update HTTP request
    * Replace "httplib" with the "requests" module
    * Change request endpoint to WordNet
    * Added query parameters
    * Verified query response

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wilkins-python
 Still in development
+
+## Requirements and Prerequisites
+Additional Libraries (via pip):
+- requests
--- wilkins.py
+++ wilkins.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 
-import httplib
+import requests
+from urllib import urlencode
 
 class Wilkins:
     """This class allows you to query words on WordNet's internet UI."""
     wn_request = None
+    parameters = None
     result = None
 
     def __init__(self):
-        self.wn_request = httplib.HTTPConnection("www.python.org")
+        self.wn_request = "http://wordnetweb.princeton.edu/perl/webwn"
+        self.parameters = dict()
         result = dict()
 
     def query_word(self, word):
-        print word
-        self.wn_request.request("HEAD", "/index.html")
-        wn_response = self.wn_request.getresponse()
-        print wn_response.status, wn_response.reason
+        self.parameters = { "s" : word, "o0" : "", "o1" : "" }
+        wn_uri = "%s?%s" % (self.wn_request, urlencode(self.parameters))
+        wn_html_result = requests.get(wn_uri)
+        print wn_html_result.text
 
 
 if __name__ == "__main__":
