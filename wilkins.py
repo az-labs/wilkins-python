a88a8f0fc71605ba125eebec24d8221115167c03
Omar Elazhary <omazhary@gmail.com>
2016-09-20 22:44:39 -0700

    Attempting to interpret response as XML

--- wilkins.py
+++ wilkins.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
 import requests
+import tempfile
 from urllib import urlencode
+from xml.dom import minidom
 
 class Wilkins:
     """This class allows you to query words on WordNet's internet UI."""
     wn_request = None
+    html_parser = None
     parameters = None
     result = None
 
@@ -18,7 +21,12 @@ class Wilkins:
         self.parameters = { "s" : word, "o0" : "", "o1" : "" }
         wn_uri = "%s?%s" % (self.wn_request, urlencode(self.parameters))
         wn_html_result = requests.get(wn_uri)
-        print wn_html_result.text
+        temp_response = tempfile.NamedTemporaryFile()
+        temp_response.write(wn_html_result.text.encode("ascii"))
+        print temp_response.name
+        html_response = minidom.parse(temp_response.name)
+        html_body = html_response.getElementsByTagName("body")
+        print html_response
 
 
 if __name__ == "__main__":
