1292f44bdaee8bf4a7f318fd7a0bc02cdfdced2e
Omar Elazhary <omazhary@gmail.com>
2016-09-23 23:28:35 -0700

    Added HTML parsing

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Still in development
 ## Requirements and Prerequisites
 Additional Libraries (via pip):
 - requests
+- beautifulsoup4 -> See the [Beautiful Soup Documentation](https://www.crummy.com/software/BeautifulSoup/bs4/doc/) here.
--- wilkins.py
+++ wilkins.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
 import requests
-import tempfile
+from bs4 import BeautifulSoup
 from urllib import urlencode
-from xml.dom import minidom
 
 class Wilkins:
     """This class allows you to query words on WordNet's internet UI."""
@@ -21,12 +20,7 @@ class Wilkins:
         self.parameters = { "s" : word, "o0" : "", "o1" : "" }
         wn_uri = "%s?%s" % (self.wn_request, urlencode(self.parameters))
         wn_html_result = requests.get(wn_uri)
-        temp_response = tempfile.NamedTemporaryFile()
-        temp_response.write(wn_html_result.text.encode("ascii"))
-        print temp_response.name
-        html_response = minidom.parse(temp_response.name)
-        html_body = html_response.getElementsByTagName("body")
-        print html_response
+        soup = BeautifulSoup(wn_html_result.text.encode("ascii"), 'html.parser')
 
 
 if __name__ == "__main__":
