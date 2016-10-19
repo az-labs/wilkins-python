7582c4f89b53bcc08a210bfffd281ded793f5c39
Omar Elazhary <omazhary@gmail.com>
2016-10-19 15:38:33 -0700

    Added rudimentary testing support.

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Still in development
 Additional Libraries (via pip):
 - requests
 - beautifulsoup4 -> See the [Beautiful Soup Documentation](https://www.crummy.com/software/BeautifulSoup/bs4/doc/) here.
+
+And these for testing:
+- py-1.4.31
+- pytest-3.0.3
---
+++ test.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python
+
+import unittest
+import subprocess
+
+class TestWilkins(unittest.TestCase):
+
+    def test_cliusage(self):
+        output = subprocess.check_output(['./wilkins.py', '--word', 'remain', '--output', 'json'])
+        self.assertEqual(output.strip(), '{"synonyms": ["stay", "rest"], "type": "Verb", "residual": ["1 (stay%2:30:00::)", "6 (rest%2:30:00::)"]}')
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestWilkins)
+    unittest.TextTestRunner(verbosity=2).run(suite)
