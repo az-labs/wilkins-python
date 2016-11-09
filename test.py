8f3fd2cde0de7d6e5e591df6bb9f594d8e66a173
Omar Elazhary <omazhary@gmail.com>
2016-11-08 19:56:29 -0800

    [FIX] Taking into account test return value.

--- test.py
+++ test.py
@@ -2,13 +2,22 @@
 
 import unittest
 import subprocess
+import sys
+
 
 class TestWilkins(unittest.TestCase):
 
     def test_cliusage(self):
-        output = subprocess.check_output(['./wilkins.py', '--word', 'remain', '--output', 'json'])
-        self.assertEqual(output.strip(), '{"synonyms": ["bla", "rest"], "type": "Verb", "residual": ["1 (stay%2:30:00::)", "6 (rest%2:30:00::)"]}')
+        expected_result = '{'
+        expected_result += '"synonyms": ["bla", "rest"], '
+        expected_result += '"type": "Verb", '
+        expected_result += '"residual": ["1 (stay%2:30:00::)", '
+        expected_result += '"6 (rest%2:30:00::)"]}'
+        output = subprocess.check_output(['./wilkins.py', '--word', 'remain',
+                                          '--output', 'json'])
+        self.assertEqual(output.strip(), expected_result)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestWilkins)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    ret_val = unittest.TextTestRunner(verbosity=2).run(suite).wasSuccessful()
+    sys.exit(not ret_val)
